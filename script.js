0d6991dfb98f498a44d6b31228f29e89ca104187
M.Kristóf <drzero123456@gmail.com>
2023-08-02 19:41:55 +0200

    animation added

--- script.js
+++ script.js
@@ -1,28 +1,102 @@
-const DAYS_ELEM = document.querySelector(`[data-time="days"]`)
-const HOURS_ELEM = document.querySelector(`[data-time="hours"]`)
-const MINUTES_ELEM = document.querySelector(`[data-time="minutes"]`)
-const SECONDS_ELEM = document.querySelector(`[data-time="seconds"]`)
 
-const timeDiff = new Date();
 
-timeDiff.setDate(timeDiff.getDate() + 14);
 
-console.log(timeDiff)
 
-let x = setInterval(() => {
-    let now = new Date().getTime();
+function CountdownTracker(label, value){
 
-    let distance = timeDiff - now;
+  var el = document.createElement('span');
 
-    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  el.className = 'flip-clock__piece';
+  el.innerHTML = '<b class="flip-clock__card card"><b class="card__top"></b><b class="card__bottom"></b><b class="card__back"><b class="card__bottom"></b></b></b>' + 
+    '<span class="flip-clock__slot">' + label + '</span>';
 
-    DAYS_ELEM.querySelector("h2").textContent = days
-    HOURS_ELEM.querySelector("h2").textContent =hours
-    MINUTES_ELEM.querySelector("h2").textContent = minutes
-    SECONDS_ELEM.querySelector("h2").textContent = seconds
-    console.log(seconds)
+  this.el = el;
 
-},1000)
+  var top = el.querySelector('.card__top'),
+      bottom = el.querySelector('.card__bottom'),
+      back = el.querySelector('.card__back'),
+      backBottom = el.querySelector('.card__back .card__bottom');
+
+  this.update = function(val){
+    val = ( '0' + val ).slice(-2);
+    if ( val !== this.currentValue ) {
+      
+      if ( this.currentValue >= 0 ) {
+        back.setAttribute('data-value', this.currentValue);
+        bottom.setAttribute('data-value', this.currentValue);
+      }
+      this.currentValue = val;
+      top.innerText = this.currentValue;
+      backBottom.setAttribute('data-value', this.currentValue);
+
+      this.el.classList.remove('flip');
+      void this.el.offsetWidth;
+      this.el.classList.add('flip');
+    }
+  }
+  
+  this.update(value);
+}
+
+// Calculation adapted from https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  return {
+    'Total': t,
+    'Days': Math.floor(t / (1000 * 60 * 60 * 24)),
+    'Hours': Math.floor((t / (1000 * 60 * 60)) % 24),
+    'Minutes': Math.floor((t / 1000 / 60) % 60),
+    'Seconds': Math.floor((t / 1000) % 60)
+  };
+}
+
+function Clock(countdown,callback) {
+  
+  countdown = countdown ? new Date(Date.parse(countdown)) : false;
+  callback = callback || function(){};
+  
+  var updateFn = countdown ? getTimeRemaining : getTime;
+
+  this.el = document.createElement('div');
+  this.el.className = 'flip-clock';
+
+  var trackers = {},
+      t = updateFn(countdown),
+      key, timeinterval;
+
+  for ( key in t ){
+    if ( key === 'Total' ) { continue; }
+    trackers[key] = new CountdownTracker(key, t[key]);
+    this.el.appendChild(trackers[key].el);
+  }
+
+  var i = 0;
+  function updateClock() {
+    timeinterval = requestAnimationFrame(updateClock);
+    
+    // throttle so it's not constantly updating the time.
+    if ( i++ % 10 ) { return; }
+    
+    var t = updateFn(countdown);
+    if ( t.Total < 0 ) {
+      cancelAnimationFrame(timeinterval);
+      for ( key in trackers ){
+        trackers[key].update( 0 );
+      }
+      callback();
+      return;
+    }
+    
+    for ( key in trackers ){
+      trackers[key].update( t[key] );
+    }
+  }
+
+  setTimeout(updateClock,500);
+}
+
+var deadline = new Date(Date.parse(new Date()) + 14 * 24 * 60 * 60 * 1000);
+var c = new Clock(deadline, function(){ alert('countdown complete') });
+document.querySelector("main").appendChild(c.el);
+
